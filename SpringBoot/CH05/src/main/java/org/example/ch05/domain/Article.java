3d7415e82eb8c99030d5d7780e353966d8742d46
kingsuhyeon <dvlproh@gmail.com>
2024-09-10 10:54:44 +0900

    CH05 시작

---
+++ SpringBoot/CH05/src/main/java/org/example/ch05/domain/Article.java
@@ -0,0 +1,29 @@
+package org.example.ch05.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
